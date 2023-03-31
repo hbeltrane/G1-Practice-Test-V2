b16bd5c2dfd4cff7388be9e87239ebe2b2b0987d
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-30 23:47:24 -0400

    Refactor controller variable names

--- backend/controllers/test.controllers.js
+++ backend/controllers/test.controllers.js
@@ -1,11 +1,11 @@
 const Question = require('../models/test.model.js');
 
-// Create and Save a new Note
+// Create and Save a new Question
 exports.create = (req, res) => {
     // Validate request
     if(!req.body.content) {
         return res.status(400).send({
-            message: "Note content can not be empty"
+            message: "Question content can not be empty"
         });
     }
 
@@ -18,20 +18,20 @@ exports.create = (req, res) => {
         });
     }
 
-    // Create a Note
+    // Create a Question
     const question = new Question({
-        title: req.body.title || "Untitled Note", 
+        title: req.body.title || "Untitled Question", 
         content: req.body.content,
         topic: req.body.topic
     });
 
-    // Save Note in the database
+    // Save Question in the database
     question.save()
     .then(data => {
         res.send(data);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
+            message: err.message || "Some error occurred while creating the Question."
         });
     });
 };
@@ -72,79 +72,79 @@ exports.findAllSign = (req, res) => {
     });
 };
 
-// Find a single question with a qId
+// Find a single question with a num
 exports.findOne = (req, res) => {
-    Question.findById(req.params.qId)
-    .then(note => {
-        if(!note) {
+    Question.findById(req.params.num)
+    .then(question => {
+        if(!question) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });            
         }
-        res.send(note);
+        res.send(question);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });                
         }
         return res.status(500).send({
-            message: "Error retrieving note with id " + req.params.qId
+            message: "Error retrieving question with id " + req.params.num
         });
     });
   };
 
-// Update a note identified by the qId in the request
+// Update a question identified by the num in the request
 exports.update = (req, res) => {
     // Validate Request
     if(!req.body.content) {
         return res.status(400).send({
-            message: "Note content can not be empty"
+            message: "Question content can not be empty"
         });
     }
 
-    // Find note and update it with the request body
-    Question.findByIdAndUpdate(req.params.qId, {
-        title: req.body.title || "Untitled Note",
+    // Find question and update it with the request body
+    Question.findByIdAndUpdate(req.params.num, {
+        title: req.body.title || "Untitled Question",
         content: req.body.content
     }, {new: true})
-    .then(note => {
-        if(!note) {
+    .then(question => {
+        if(!question) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });
         }
-        res.send(note);
+        res.send(question);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });                
         }
         return res.status(500).send({
-            message: "Error updating note with id " + req.params.qId
+            message: "Error updating question with id " + req.params.num
         });
     });
 };
 
-// Delete a note with the specified qId in the request
+// Delete a question with the specified num in the request
 exports.delete = (req, res) => {
-    Question.findByIdAndRemove(req.params.qId)
-    .then(note => {
-        if(!note) {
+    Question.findByIdAndRemove(req.params.num)
+    .then(question => {
+        if(!question) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });
         }
-        res.send({message: "Note deleted successfully!"});
+        res.send({message: "Question deleted successfully!"});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.qId
+                message: "Question not found with id " + req.params.num
             });                
         }
         return res.status(500).send({
-            message: "Could not delete note with id " + req.params.qId
+            message: "Could not delete question with id " + req.params.num
         });
     });
 };
