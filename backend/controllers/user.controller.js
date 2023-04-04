d8a6fec4be242e524f1877b872359c7186aecdcc
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-04-04 01:17:05 -0400

    Implemented login functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,25 +11,27 @@ const User = require('../models/user.model.js');
 // Create and Save a new User
 exports.create = (req, res) => {
     // Validate request
-    /*
-    if (!req.body.content) {
+    
+    if (!req.body) {
         return res.status(400).send({
-            message: "User content can not be empty"
+            message: "User fields can not be empty"
         });
-    }*/
+    }
 
     // Create a User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        password: req.body.password
+        password: req.body.password,
+        role: 'user'
     });
 
     // Save User in the database
     user.save()
         .then(data => {
-            res.send(data);
+            // res.status(201).send("User successfully created");
+            res.render('login');
         }).catch(err => {
             res.status(500).send({
                 message: err.message || "Some error occurred while creating the User."
@@ -37,7 +39,39 @@ exports.create = (req, res) => {
         });
 };
 
-// Find a single user with a num
+// Validate user's credentials
+exports.authenticate = async (req, res) => {
+  
+  let user = await User.findOne({ "email": req.body.email });
+  console.log(user);
+
+  if (user != null) {
+
+    if (req.body.password == user.password) {
+      req.session.user = user.email;
+      if (user.role == 'admin') {
+        // res.redirect('/router/dashboard');
+      } else {
+        res.end("Login successful");
+      }
+
+    } else {
+      res.status(400).send({
+        message: "Invalid username or password"
+      });
+      // res.redirect('/auth');
+    }
+
+  } else {   
+    res.status(400).send({
+      message: "Invalid username or password"
+    });
+    // res.redirect('/auth');
+  }
+  
+};
+
+// Find a single user with a email
 exports.findOne = (req, res) => {
     User.find({ "email": req.params.Email })
     .then(users => {
