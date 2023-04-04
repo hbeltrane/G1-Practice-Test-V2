ef91b232db60d477b67a97a6608b6b2c43d9822b
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-04-04 01:13:14 -0400

    Removed unnecessary code

--- router.js
+++ router.js
@@ -1,21 +1,14 @@
-/*
-        Lambton College
-        CSD 3103 - Full Stack JavaScript
-        Term project
-        Hugo Beltran Escarraga - C0845680
-        Juan Luis Casanova Romero - C0851175
-*/
+/** 
+ * Lambton College
+ * CSD 3103 - Full Stack JavaScript
+ * Term project
+ * Hugo Beltran Escarraga - C0845680
+ * Juan Luis Casanova Romero - C0851175
+ */
 
 var express = require('express');
 var router = express.Router();
-var loggeduser;
 
-/*
-const credential = {
-    email: "admin@ferrari.com",
-    password: "enzo"
-}
-*/
 //route for index
 router.get('/index', (req, res) => {
     res.render('index');
@@ -41,27 +34,6 @@ router.get('/result', (req, res) => {
     res.render('result');
 });
 
-// login user
-router.post('/login', async (req, res) => {
-    // Sign up
-    if (req.body.password2 != null) {
-        req.session.user = req.body.email;
-        res.redirect('/router/dashboard');
-    // Log in
-    } else {
-        let requestUser = await fetch('http://localhost:3000/users/' + req.body.email);
-        loggeduser = requestUser.json();
-        console.log(loggeduser.name);
-        if (req.body.email == loggeduser.email && req.body.password == loggeduser.password) {
-            req.session.user = loggeduser.email;
-            res.redirect('/router/dashboard');
-            //res.end("Login successful");
-        } else {
-            res.end("Invalid username or password");
-        }
-    }
-});
-
 //route for dashboard
 router.get('/dashboard', (req, res) => {/*
     if (req.session.user) {*/
@@ -81,6 +53,6 @@ router.get('/logout', (req, res) => {
             res.render('base', { title: "Express", logout: "Logout successful" });
         }
     });
-})
+});
 
 module.exports = router;
