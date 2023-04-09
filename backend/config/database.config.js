3e3c271d47ae74bcc7574d5ada13c69de776a821
hbeltrane <hbeltrane@hotmail.com>
2023-04-09 11:59:20 -0400

    Set new db server and host url updated to Azure

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -6,9 +6,10 @@
 		Juan Luis Casanova Romero - C0851175
 */
 
-const password = process.env.PRACTICE_TEST
-console.log(process.env.JAVA_HOME)
+//const password = process.env.PRACTICE_TEST
+
 module.exports = {
-    //url: 'mongodb://0.0.0.0:27017/G1-Practice-Test'
-    url:'mongodb+srv://admin:${password}@fullstackjs.cbyf9va.mongodb.net/PracticeTest'
+    //dburl: 'mongodb://0.0.0.0:27017/G1-Practice-Test'
+    //dburl:'mongodb+srv://admin:${password}@fullstackjs.cbyf9va.mongodb.net/PracticeTest'
+	dburl:'mongodb+srv://admin:AdminSecret@csd3103.66klf4s.mongodb.net/?retryWrites=true&w=majority'
   }
