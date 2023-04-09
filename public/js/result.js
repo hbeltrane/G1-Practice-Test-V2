034599c0dfa4e3900b43abaeeb004e135537e35d
hbeltrane <hbeltrane@hotmail.com>
2023-04-09 14:04:04 -0400

    Alert message test

--- public/js/result.js
+++ public/js/result.js
@@ -1,9 +1,9 @@
 /*
-		Lambton College
-		CSD 3103 - Full Stack JavaScript
-		Term project
-		Hugo Beltran Escarraga - C0845680
-		Juan Luis Casanova Romero - C0851175
+    Lambton College
+    CSD 3103 - Full Stack JavaScript
+    Term project
+    Hugo Beltran Escarraga - C0845680
+    Juan Luis Casanova Romero - C0851175
  */
 
 var totalRules;
@@ -22,31 +22,31 @@ const resultAlert = document.querySelector("#alert");
 const alertMessage = document.querySelector("#message");
 
 function createChart() {
-    var xValues = ["Rules - Correct", "Signs- Correct", "Signs - Incorrect", "Rules - Incorrect"];
-    var yValues = [correctRules, correctSigns, incorrectSigns, incorrectRules];
-    var barColors = [
+  var xValues = ["Rules - Correct", "Signs- Correct", "Signs - Incorrect", "Rules - Incorrect"];
+  var yValues = [correctRules, correctSigns, incorrectSigns, incorrectRules];
+  var barColors = [
     "#01A775",
     "#6495ED",
     "#FFC300",
-    "#db2e3f" 
-    ];
+    "#DB2E3f"
+  ];
 
-    new Chart("myChart", {
+  new Chart("myChart", {
     type: "pie",
     data: {
-        labels: xValues,
-        datasets: [{
+      labels: xValues,
+      datasets: [{
         backgroundColor: barColors,
         data: yValues
-        }]
+      }]
     },
     options: {
-        title: {
+      title: {
         display: true,
         text: "Driver License Knowledge Practice Test Results"
-        }
+      }
     }
-    });
+  });
 }
 
 function round(input) {
@@ -65,27 +65,31 @@ function messageFail() {
 }
 
 function displayTotals() {
-    incorrectRules = totalRules - correctRules;
-    incorrectSigns = totalSigns - correctSigns;
-    total = totalRules + totalSigns;
-    correctTotal = correctRules + correctSigns;
-    incorrectTotal = incorrectRules + incorrectSigns;
-    document.getElementById("correct-rules").innerHTML = correctRules;
-    document.getElementById("incorrect-rules").innerHTML = incorrectRules;
-    document.getElementById("correct-signs").innerHTML = correctSigns;
-    document.getElementById("incorrect-signs").innerHTML = incorrectSigns;
-    document.getElementById("correct-total").innerHTML = correctTotal;
-    document.getElementById("incorrect-total").innerHTML = incorrectTotal;
-    document.getElementById("percentage-rules").innerHTML = round(100 * correctRules / totalRules) + "%";
-    document.getElementById("percentage-signs").innerHTML = round(100 * correctSigns / totalSigns) + "%";
-    let percentageTotal = 100 * correctTotal / total
-    document.getElementById("percentage-total").innerHTML = round(percentageTotal) + "%";
+  incorrectRules = totalRules - correctRules;
+  incorrectSigns = totalSigns - correctSigns;
+  total = totalRules + totalSigns;
+  correctTotal = correctRules + correctSigns;
+  incorrectTotal = incorrectRules + incorrectSigns;
+  document.getElementById("correct-rules").innerHTML = correctRules;
+  document.getElementById("incorrect-rules").innerHTML = incorrectRules;
+  document.getElementById("correct-signs").innerHTML = correctSigns;
+  document.getElementById("incorrect-signs").innerHTML = incorrectSigns;
+  document.getElementById("correct-total").innerHTML = correctTotal;
+  document.getElementById("incorrect-total").innerHTML = incorrectTotal;
+  document.getElementById("percentage-rules").innerHTML = round(100 * correctRules / totalRules) + "%";
+  document.getElementById("percentage-signs").innerHTML = round(100 * correctSigns / totalSigns) + "%";
+  let percentageTotal = 100 * correctTotal / total
+  document.getElementById("percentage-total").innerHTML = round(percentageTotal) + "%";
 
-    if (percentageTotal >= 80) {
-      messageSuccess();
-    } else {
-      messageFail();
-    }
+  if (percentageTotal >= 80) {
+    messageSuccess();
+  } else {
+    messageFail();
+  }
+  setTimeout(() => {
+    resultAlert.hidden = true;
+    document.querySelector("#result").classList.add("pt-4");
+  }, 5000);
 }
 
 window.addEventListener('load', (event) => {
@@ -95,10 +99,4 @@ window.addEventListener('load', (event) => {
   correctSigns = Number(sessionStorage.getItem("correct-signs"));
   displayTotals();
   createChart();
-  
-  resultAlert.addEventListener('closed.bs.alert', event => {
-    resultAlert.display = "none";
-    resultAlert.classList.remove("show");
-    document.querySelector("#result").classList.add("pt-4");
-  });
 });
