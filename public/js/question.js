2e9034aafca01eb1fcf9cbea10274a3063af6ef1
hbeltrane <hbeltrane@hotmail.com>
2023-04-09 02:13:53 -0400

    File upload

--- public/js/question.js
+++ public/js/question.js
@@ -1,3 +1,22 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(form);
+
+    const urlEncoded = new URLSearchParams(fd).toString();
+
+    fetch('http://localhost:3000/questions', {
+        method: "POST",
+        body: fd
+    })
+    alert("Question saved");//Replace
+    form.reset();
+    document.querySelector("#text").hidden = false;
+    document.querySelector("#question").required = true;
+    document.querySelector("#ref").hidden = true;
+})
+
 function setRef() {
     if (document.querySelector("#category").value == "Rules") {
         document.querySelector("#text").hidden = false;
